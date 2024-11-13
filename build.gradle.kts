474e5b824ecf0e27a67511779e0dc45d46f052d4
Shamkhani <shamkhani.mehdi@golrang.com>
2024-11-13 18:08:25 +0330

    Initial commit

---
+++ build.gradle.kts
@@ -0,0 +1,31 @@
+plugins {
+    `java-gradle-plugin`
+    `kotlin-dsl`
+     signing
+}
+
+kotlin {}
+
+gradlePlugin {
+    plugins {
+        register("configGenerator") {
+            id = "config.generator.plugin-multiplatform"
+            implementationClass = "config.generator.plugin.ConfigGeneratorPlugin"
+            displayName = "MultiPlatform Config Generator Plugin "
+            description =
+                "A Gradle plugin to generate a Kotlin object within desired properties for using in  KMP project configurations\n @MehdiShamkhani"
+            tags = listOf("KMP", "kotlin-multiplatform", "build-config", "config-generator")
+        }
+    }
+}
+
+
+repositories {
+    mavenCentral()
+    gradlePluginPortal()
+}
+
+
+
+
+
